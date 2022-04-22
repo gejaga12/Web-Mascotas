4a061ddf860413152f02f82b942cec16f44e9ec7
gejaga12 <gejaga12@hotmail.com>
2022-04-22 09:05:15 -0300

    Carrito de compras

---
+++ stock.js
@@ -0,0 +1,13 @@
+let stockProductos = [
+    {id: 1, nombre: "Artemisa", tipo: "buzo", cantidad: 1, desc: "Una gata piola", precio: 0, talle: "Chiquicita", img: './img/art1.jpeg'},
+    {id: 2, nombre: "Fuma", tipo: "buzo", cantidad: 1, desc: "Una maquina de hacer cagadas", precio: 70000, talle: "Gordita", img: './img/art2.jpeg'},
+    {id: 3, nombre: "Mora", tipo: "buzo", cantidad: 1, desc: "La perra perfecta", precio: 200000, talle: "Chiquicita", img: './img/art4.jpeg'},
+    {id: 4, nombre: "Novak", tipo: "buzo", cantidad: 1, desc: "Su mejor habilidad es dormir", precio: 30000, talle: "Gordito", img: './img/art5.jpeg'},
+    {id: 5, nombre: "Toro", tipo: "buzo", cantidad: 1, desc: "Perro más vago que el dueño", precio: 1400, talle: "Flaco", img: './img/art3.jpeg'},
+    {id: 6, nombre: "Dinara", tipo: "buzo", cantidad: 1, desc: "La gata maceta", precio: 150000, talle: "Chiquicita", img: './img/art6.jpeg'},
+    {id: 7, nombre: "Pepa", tipo: "remera", cantidad: 1, desc: "Directamente traida desde Francia", precio: 500, talle: "Gordita", img: './img/remera.jpg'},
+    {id: 8, nombre: "Betun", tipo: "remera", cantidad: 1, desc: "La definicion de la vagancia", precio: 500, talle: "Flaco", img: './img/remera.jpg'},
+    
+]
+
+
